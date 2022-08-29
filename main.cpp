26824d0a1b575ec3c94a7bf0de684ae8a31dcefa
Saraf Anshul <anshulsaraf@sharechat.co>
2022-08-29 20:23:35 +0530

    add : draw embedded triangle using index buffer

--- main.cpp
+++ main.cpp
@@ -32,25 +32,6 @@ int main() {
     // create window
     GLFWwindow* window = glfwCreateWindow(800, 800, "test window", nullptr, nullptr);
 
-
-    float r = 0.5, rx = 0, ry = 0;
-    float pi = 3.14159f;
-    int points = 10;
-
-    GLfloat vertices[3*(points + 1)];
-    for(int i = 0 ; i <= points ; i++){
-        float x , y , z = 0;
-
-        float theta=(2*pi*(float)i)/(float)points;
-
-        x = r * cos(theta) + rx;
-        y = r * sin(theta) + ry;
-
-        vertices[i*3]     = x ;
-        vertices[i*3 + 1] = y ;
-        vertices[i*3 + 2] = z ;
-    }
-
     if(!window){
         std::cout << "Unable to create window :(\n";
         glfwTerminate();
@@ -65,6 +46,32 @@ int main() {
 
     // specify viewport
     glViewport(0, 0, 800, 800);
+
+    /*         2
+     *        /\
+     *     3 /__\ 4
+     *     /\   /\
+     *   /___\/___\
+     *  0    5     1
+     *   using index buffer to reuse draw points at intersection
+     *   we draw 0, 1, 2 | 3, 4, 5
+     *   and specify index buffer we determine order
+     *   [0, 3, 5] , [5, 4, 1], [3, 2, 4], we repeat same vertices twice.
+     */
+    GLfloat vertices[] ={
+            -0.5, -0.5, 0.0, // lower left
+            0.5, -0.5, 0.0, // lower right
+            0.0,  0.5, 0.0, // upper corner
+            -0.5/2, 0.0, 0.0, // inner left
+            0.5/2, 0.0, 0.0, // inner right
+            0.0, -0.5, 0.0, // inner down
+    };
+
+    GLuint indices[] = {
+        0, 3, 5, // lower left triangle
+        3, 4, 2, // upper triangle
+        5, 4, 1 // lower right triangle
+    };
 
     // create shader and attach source -> compile shader
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -83,14 +90,18 @@ int main() {
 
 
     // VBO
-    GLuint VBO[1], VAO[1];
+    GLuint VBO[1], VAO[1], EBO[1];
     glGenVertexArrays(1, VAO); // before VBO
     glGenBuffers(1, VBO);
+    glGenBuffers(1, EBO);
 
     glBindVertexArray(VAO[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // tells how to read vertex data.
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)nullptr );
@@ -98,6 +109,7 @@ int main() {
 
     glBindBuffer(GL_ARRAY_BUFFER, 0); // ordering is imp.
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // unbind after VAO
 
 
     // poll window
@@ -109,7 +121,7 @@ int main() {
 
         glBindVertexArray(VAO[0]);
         glPointSize( 20.0 ); // for drawing points
-        glDrawArrays(GL_LINE_LOOP, 0, sizeof(vertices)/(3*sizeof(float)));
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
 
@@ -121,6 +133,7 @@ int main() {
     glDeleteShader(fragmentShader);
     glDeleteVertexArrays(1, VAO);
     glDeleteBuffers(1, VBO);
+    glDeleteBuffers(1, EBO);
     glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
